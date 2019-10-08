b56a8a92c073b3dd227e0b2552ef0eb2d9ed8757
Nzix <iminezn5656@outlook.com>
2019-10-08 20:24:38 +0800

    leancloud support

---
+++ server.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.urlencoded({extended: false}))
+
+require(path.join(__dirname,'now')).routes.forEach(route =>
+	app.use(new RegExp(route.src), require(path.join(__dirname, route.dest)))
+)
+
+app.listen(parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000))
