e7e99ee0767084d2719b605369ca7c082c9254e1
Nzix <iminezn5656@outlook.com>
2019-10-10 15:33:44 +0800

    I'm spent

--- match.js
+++ match.js
@@ -1,20 +1,37 @@
-const match = require('@nondanee/unblockneteasemusic')
+// const match = require('@nondanee/unblockneteasemusic')
+
+const find = require('@nondanee/unblockneteasemusic/provider/find')
+const qq = require('@nondanee/unblockneteasemusic/provider/qq')
 
 module.exports = (req, res) => {
 	let id = (req.body || {}).id
 	return (id && !isNaN(id) ?
-		match(id, ['qq'])
-		.then(song => [Object.assign(song, {
+		find(id).then(qq.check)
+		.then(url => ({
+			url,
+			size: 0,
 			code: 200,
+			br: 128000,
 			type: 'mp3',
 			id: parseInt(id),
-			br: song.br || 128000,
 			md5: Array(32 - id.length + 1).join('0') + id,
 			matchedPlatform: 'QQ音乐',
 			matchedSongName: id + '_FROM_QQ',
 			matchedArtistName: '',
 			matchedDuration: false
-		})])
+		}))
+		// match(id, ['qq'])
+		// .then(song => [Object.assign(song, {
+		// 	code: 200,
+		// 	type: 'mp3',
+		// 	id: parseInt(id),
+		// 	br: song.br || 128000,
+		// 	md5: Array(32 - id.length + 1).join('0') + id,
+		// 	matchedPlatform: 'QQ音乐',
+		// 	matchedSongName: id + '_FROM_QQ',
+		// 	matchedArtistName: '',
+		// 	matchedDuration: false
+		// })])
 		.catch(() => null) : Promise.resolve(null)
 	)
 	.then(data => ({code: data ? 200 : 404, data}))
