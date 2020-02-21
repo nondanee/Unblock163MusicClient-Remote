c44e24347c7aba69995b66d265c3c8fb6b634f26
nondanee <iminezn5656@outlook.com>
2020-02-21 23:19:45 +0800

    drop express

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
-const resolvePath = (...path) => require('path').join.apply(null, [__dirname].concat(path))
-
-const bodyParse = text => {
-  let body = {}
-  try { body = JSON.parse(text) } catch(error) { body = require('querystring').parse(text || '') }
-  return body
-}
-
-const routes = require(resolvePath('now')).routes.map(route =>
-  ({ path: route.src + '$', handler: require(resolvePath(route.dest)) })
-)
-
-const target = path => routes.find(route => (new RegExp(route.path)).test(path))
-
-const dock = (req, handler) => new Promise(resolve => {
-  let status = 200
-  const res = { status: code => (status = code, res), json: body => resolve({ status, body }) }
-  handler(req, res).catch(error => resolve({ status: 500, body: error }))
-})
+const { parse, target, dock } = require('./server')
 
 module.exports.main = async (event, context, callback) => {
   const path = event.path.replace(event.requestContext.path, '') || '/'
   const route = target(path)
-  const req = { body: bodyParse(event.body), headers: event.headers, query: event.queryString, path }
+  const req = { body: parse(event.body), headers: event.headers, query: event.queryString, path }
   console.log('req', req)
   const output = await dock(req, route.handler)
   return output.body
@@ -31,7 +13,7 @@ module.exports.handler = (req, res, context) => {
   const { path, headers } = req
   const route = target(path)
   require('raw-body')(req, (error, body) =>
-    dock({ body: bodyParse(body), headers, query: req.queries, path }, route.handler)
+    dock({ body: parse(body), headers, query: req.queries, path }, route.handler)
       .then(output => (res.setHeader('content-type', 'application/json'), res.send(JSON.stringify(output.body))))
   )
 }
--- server.js
+++ server.js
@@ -1,13 +1,35 @@
-const path = require('path')
-const express = require('express')
-const bodyParser = require('body-parser')
+const { read } = require('@nondanee/unblockneteasemusic/src/request')
 
-const app = express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+const land = (...path) => require('path').join.apply(null, [__dirname].concat(path))
 
-require(path.join(__dirname, 'now')).routes.forEach(route =>
-  app.use(new RegExp(route.src), require(path.join(__dirname, route.dest)))
+const routes = require(land('now')).routes.map(route =>
+  ({ path: route.src + '$', handler: require(land(route.dest)) })
 )
 
-app.listen(parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000))
+const target = path => routes.find(route => (new RegExp(route.path)).test(path))
+
+const parse = text => {
+  let body = {}
+  try { body = JSON.parse(text) } catch(error) { body = require('querystring').parse(text || '') }
+  return body
+}
+
+const dock = (req, handler) => new Promise(resolve => {
+  let status = 200
+  const res = { status: code => (status = code, res), json: body => resolve({ status, body }) }
+  handler(req, res).catch(error => resolve({ status: 500, body: error }))
+})
+
+const entry = (req, res) => {
+  const { pathname, search } = require('url').parse(req.url)
+  const route = target(pathname)
+  const data = { query: parse(search.slice(1)), headers: req.headers, path: pathname }
+  read(req)
+    .then(body => dock(Object.assign(data, { body: parse(body) }), route.handler))
+    .then(output => (res.writeHead(200, { 'content-type': 'application/json' }), res.end(JSON.stringify(output.body))))
+    .catch()
+}
+
+module.exports = { parse, target, dock }
+
+if (require.main === module) require('http').createServer(entry).listen(parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000))
